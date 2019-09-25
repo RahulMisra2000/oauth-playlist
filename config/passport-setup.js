9a9974f73497a6c8eb00cd34ba85a82cb9b82cec
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-09-25 16:11:08 -0400

    I believe mongo

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,8 +4,8 @@ const keys = require('./keys');
 const User = require('../models/user-model');
 
 
-//*** Remember since we are persisting all users in a mongoDB and it creates a unique key for each record added. It is that 
-//    key that we are using to uniquely idenitfy a user
+//*** Remember since we are persisting all users in a mongoDB and it creates a unique key (called _id) for each record added. It is that 
+//    key that we are using to uniquely idenitfy a user. I believe id is an alias for _id
 passport.serializeUser((user, done) => {
     done(null, user.id);                    //*** The 2nd parameter is shoved inside a cookie
 });
