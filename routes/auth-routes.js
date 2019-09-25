021bd5286c78b5a6e6ce5425264f198165d9a4f9
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-09-25 13:26:34 -0400

    passport.authenticate(strategy) is how passport makes http calls to the identity providers (aka strategy)

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -13,15 +13,21 @@ router.get('/logout', (req, res) => {
 });
 
 // auth with google+
-router.get('/google', passport.authenticate('google', {
-    scope: ['profile']
-}));
+//**************** The scope we are requesting ****************************
+router.get('/google', passport.authenticate('google', {scope: ['profile']}));
 
-// callback route for google to redirect to
-// hand control to passport to use code to grab profile info
+
+// callback route for google to redirect to hand control to passport to use code to grab profile info
+//********* Google will call this endpoint. This is the Authorized Redirect URI we specified both in the Google Developer Console and
+//          also when configuring the Google Strategy in another file in this project
+//          Google will send the Authrization code as a query string. When passport.authenticate() sees a code in the query string
+//          it no longer takes the user to the consent screen/credential entering page, as in the case if the call ABOVE, BUT instead makes 
+//          a request to google to send back the tokens. Google obliges by calling the function that was provided at the time Google 
+//          Strategy was configured
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
     // res.send(req.user);
     res.redirect('/profile');
 });
+
 
 module.exports = router;
